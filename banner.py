a66791c11eb3c83a4ca8199e0c03c358b76ee62c
KiLL3R_xRO <79046188+KiLL3R-xRO@users.noreply.github.com>
2021-12-18 18:40:03 +0530

    Create banner.py

---
+++ banner.py
@@ -0,0 +1,11 @@
+print("""███████╗██╗██████╗ ███████╗    ██╗  ██╗██╗   ██╗███╗   ██╗████████╗███████╗██████╗ 
+██╔════╝██║██╔══██╗██╔════╝    ██║  ██║██║   ██║████╗  ██║╚══██╔══╝██╔════╝██╔══██╗
+█████╗  ██║██████╔╝█████╗█████╗███████║██║   ██║██╔██╗ ██║   ██║   █████╗  ██████╔╝
+██╔══╝  ██║██╔══██╗██╔══╝╚════╝██╔══██║██║   ██║██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗
+██║     ██║██║  ██║███████╗    ██║  ██║╚██████╔╝██║ ╚████║   ██║   ███████╗██║  ██║
+╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝    ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+                                                                                   \n""")
+creator="__________________________________________________________________________________\nORIGINAL SCRIPT BY LORD XRO OF CEO-FIREBASE VULNERABILITY SCANNER TEAM U.E.\n__________________________________________________________________________________"
+dots="__________________________________________________________________________________"
+choice="[1]CHECK DATABASE VULNERABILITY\n[2]DUMP DATABASE USING JSON\n[3]READ DATA FROM REALTIME DB\n[4]VIEW OR UPLOAD MEDIA\n[5]AUTHENTICATE TO DATABASE\n[6]UPLOAD DATA TO REALTIME DATABASE\n[7]DELETE DATA FROM DATABASE\n[8]SPAM REALTIME DATABASE\n[9]HELP AND COMMANDS"
+wordlist="abcdefghijklmnopqrstuvwxyz"
